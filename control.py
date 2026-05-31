b7e7b4bcacff32d23d00d1e25a0757c324c0986a
Max Cooper <mdkoopzer1989@gmail.com>
2026-05-31 10:34:56 -0230

    Swapped keyboard for readchar; implemented threading
    Refactor camera control to use threading for key input and simplify data retrieval methods.

--- control.py
+++ control.py
@@ -1,6 +1,17 @@
 import pyrealsense2 as rs
 import numpy as np
-from keyboard import is_pressed
+import readchar as rc
+import threading as th        # built-in, no installation required
+
+key_input = None
+running = True
+
+def quitThread():
+    global key_input, running
+    while running:
+        key_input = rc.readkey()
+        if key_input == 'q':
+            running = False
 
 class RealSenseCamera:
     def __init__(self):
@@ -25,44 +36,42 @@ class RealSenseCamera:
         frames = self.control.wait_for_frames()
         depth_frame = frames.get_depth_frame()
         margins = (depth_frame.get_width(), depth_frame.get_height())
-        return depth_frame
+        return depth_frame, margins
     
-    def getDepthMargins(self, depth_frame):
-        width = depth_frame.get_width()
-        height = depth_frame.get_height()
-        margins = width, height
-        return margins
-    
-    def depthDataArray(self, depth_frame):
-        depth_data = np.asanyarray(depth_frame.get_data())
+    def depthDataArray(self):
+        depth_data = np.asanyarray(self.getDepthFrame()[0].get_data())
         return depth_data
     
-    def colorDataArray(self, color_frame):
-        color_data = np.asanyarray(color_frame.get_data())
+    def colorDataArray(self):
+        color_data = np.asanyarray(self.getColorFrame()[0].get_data())
         return color_data
-    
+
+controlThread = th.Thread(target=quitThread, daemon=True)
+controlThread.start()
+
 camera = RealSenseCamera()
 try:
     camera.start()
+    
 except RuntimeError as e:
     print(f"Error starting camera: {e}")
     exit(1)
 
 for i in range(5):
     camera.getDepthFrame()  # Buffer time for auto-exposure
-    
-while True:
-    if is_pressed('q'): break
 
-    color_frame = camera.getColorFrame()
-    depth_frame = camera.getDepthFrame()
-    margins = camera.getDepthMargins(depth_frame)
-    
-    depth_data = camera.depthDataArray(depth_frame)
-    color_data = camera.colorDataArray(color_frame)
+try:
+    while running:
+        color_frame = camera.getColorFrame()
+        depth_frame = camera.getDepthFrame()
+        margins = camera.getDepthMargins(depth_frame)
+        
+        depth_data = camera.depthDataArray(depth_frame)
+        color_data = camera.colorDataArray(color_frame)
 
-camera.stop()
-
+except KeyboardInterrupt:
+    camera.stop()
+    exit(1)
 
 
 

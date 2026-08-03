580f6b2d10dce9504414f190089c66c45faca685
Sam <samuel.bracci58@ed.cna.nl.ca>
2026-08-03 18:44:31 -0230

    Cleaned up file a little and added transitions for all states

--- state_machine.py
+++ state_machine.py
@@ -1,16 +1,66 @@
 from enum import Enum, auto
 
 class State(Enum):
-	IDLE = auto()
-	MOVE = auto()
-	SCAN = auto()
-	# add more here
+    OFF = auto()
+    IDLE = auto()
+    SEARCH = auto()
+    MOVE = auto()
+    RETURN = auto()
+    MISSION_COMPLETE = auto()
+
 
 class Machine:
-	def __init__(self):
-	   self.current_state = State.IDLE
+    def __init__(self):
+        self.current_state = State.IDLE
 
-	def transition(self):
-	   match (self.current_state, action):
+    def transition(self, action: str):
+        match (self.current_state, action):
+            #OFF --> *STATE*
+            case (State.OFF, "Setting to Autonomous"):
+                self.current_state = State.IDLE
+            case (State.OFF, "Moving around"):
+                self.current_state = State.MOVE
 
-		case #...
+            #IDLE --> *STATE*
+            case (State.IDLE, "Shutting boat off"):
+                self.current_state = State.OFF
+            case (State.IDLE, "Moving around"):
+                self.current_state = State.MOVE
+            case (State.IDLE, "Returning home"):
+                self.current_state = State.RETURN
+
+			#SEARCH --> *STATE*
+            case (State.SEARCH, "Shutting boat off"):
+                self.current_state = State.OFF
+            case (State.SEARCH, "Idleing"):
+                self.current_state = State.IDLE
+            case (State.SEARCH, "Moving around"):
+                self.current_state = State.MOVE
+            case (State.SEARCH, "Returning home"):
+                self.current_state = State.RETURN
+                
+			#MOVE --> *STATE*
+            case (State.MOVE, "Shutting boat off"):
+                self.current_state = State.OFF
+            case (State.MOVE, "Idleing"):
+                self.current_state = State.IDLE
+            case (State.MOVE, "Searching"):
+                self.current_state = State.SEARCH
+            case (State.MOVE, "Returning home"):
+                self.current_state = State.RETURN
+
+			#RETURN --> *STATE*
+            case (State.RETURN, "Shutting boat off"):
+                self.current_state = State.OFF
+            case (State.RETURN, "Idleing"):
+                self.current_state = State.IDLE
+            case (State.RETURN, "Finished Mission!"):
+                self.current_state = State.MISSION_COMPLETE
+
+			#MISSION_COMPLETE --> *STATE*
+            case (State.MISSION_COMPLETE, "Shutting boat off"):
+                self.current_state = State.OFF
+            case (State.MISSION_COMPLETE, "Idling"):
+                self.current_state = State.IDLE
+            case (State.MISSION_COMPLETE, "Returning home"):
+                self.current_state = State.RETURN
